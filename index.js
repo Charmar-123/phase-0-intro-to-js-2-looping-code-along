d9c7840d257f2a7d230a4cab641ffb4bf6628cce
Charbel Maroun <charbel-maroun@hotmail.com>
2022-11-09 22:56:51 -0500

    commit tree 6e35a8791f45e7b13e591b3a506a8657620a2930

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(names, eventCeleb){
+    const msgs = [];
+    for (let i =0; i < names.length; i++){
+        msgs.push(`Thank you, ${names[i]}, for the wonderful ${eventCeleb} gift!`)
+    }
+    return msgs
+}
+
+function countDown(num){
+
+    let i = 0;
+
+    while (i<num+1){
+        console.log(i)
+        i++;
+    }
+
+}
+
+countDown(3)
